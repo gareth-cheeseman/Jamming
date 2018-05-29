d0aeb80a2ed0c159390aa4b3d9cadf44b9594d0a
gareth-cheeseman <garethcheeseman@humbug.local>
2018-05-29 21:35:01 +0100

    Adds passing playlist name and tracks status to playlist and tracklist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -12,6 +12,12 @@ class App extends Component {
         { name: 'name1', artist: 'artist1', album: 'album1', id: '1' },
         { name: 'name2', artist: 'artist2', album: 'album2', id: '2' },
         { name: 'name3', artist: 'artist3', album: 'album3', id: '3' }
+      ],
+      playlistName : 'playlist1',
+      playlistTracks: [
+        { name: 'pname1', artist: 'partist1', album: 'palbum1', id: 'p1' },
+        { name: 'pname2', artist: 'partist2', album: 'palbum2', id: 'p2' },
+        { name: 'pname3', artist: 'partist3', album: 'palbum3', id: 'p3' }
       ]
     };
   }
@@ -25,8 +31,8 @@ class App extends Component {
         <div className="App">
           {/*Add a SearchBar component*/}
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults}/>
-            {/*Add a Playlist component*/}
+            <SearchResults searchResults={this.state.searchResults} />
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
           </div>
         </div>
       </div>
