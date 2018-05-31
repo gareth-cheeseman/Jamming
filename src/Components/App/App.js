86aabe1c318506b2730a84f3c4f268d6fd4d4ca0
Gareth Cheeseman <Gareth.Cheeseman@stormid.com>
2018-05-31 14:19:11 +0100

    Finished save playlist method; works, mostly

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -9,53 +9,9 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchResults: [
-        {
-          name: 'name1',
-          artist: 'artist1',
-          album: 'album1',
-          id: '1',
-          uri: 'u1'
-        },
-        {
-          name: 'name2',
-          artist: 'artist2',
-          album: 'album2',
-          id: '2',
-          uri: 'u2'
-        },
-        {
-          name: 'name3',
-          artist: 'artist3',
-          album: 'album3',
-          id: '3',
-          uri: 'u3'
-        }
-      ],
-      playlistName: 'playlist1',
-      playlistTracks: [
-        {
-          name: 'pname1',
-          artist: 'partist1',
-          album: 'palbum1',
-          id: 'p1',
-          uri: 'pu1'
-        },
-        {
-          name: 'pname2',
-          artist: 'partist2',
-          album: 'palbum2',
-          id: 'p2',
-          uri: 'pu2'
-        },
-        {
-          name: 'pname3',
-          artist: 'partist3',
-          album: 'palbum3',
-          id: 'p3',
-          uri: 'pu3'
-        }
-      ]
+      searchResults: [],
+      playlistName: '',
+      playlistTracks: []
     };
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
@@ -89,6 +45,11 @@ class App extends Component {
 
   savePlaylist() {
     const trackURIs = this.state.playlistTracks.map(track => track.uri);
+    Spotify.savePlaylist(this.state.playlistName, trackURIs);
+    this.setState({
+      playlistName: 'New Playlist',
+      playlistTracks: []
+    });
   }
 
   search(searchTerm) {
--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,18 +1,29 @@
 const clientId = '554f083f9c764ed1825048cc595775bd';
 const redirectURI = 'http://localhost:3000/';
+const baseURL = 'https://api.spotify.com/v1';
 let accessToken;
 const Spotify = {
+  handleResponse(response) {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error(`Error response: ${response.statusText}`);
+  },
+
+  handleNetworkError(networkError) {
+    console.log(networkError.message);
+  },
+
   getAccessToken() {
     if (accessToken) {
       return accessToken;
     }
     const url = window.location.href;
     if (url.includes('access_token')) {
-      let token = url.match(/access_token=([^&]*)/);
-      const expiresIn = url.match(/expires_in=([^&]*)/);
+      accessToken = url.match(/access_token=([^&]*)/)[1];
+      const expiresIn = url.match(/expires_in=([^&]*)/)[1];
       window.setTimeout(() => (accessToken = ''), expiresIn * 1000);
       window.history.pushState('Access Token', null, '/');
-      accessToken = token[1];
       return accessToken;
     } else {
       window.location = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
@@ -20,17 +31,11 @@ const Spotify = {
   },
 
   search(searchTerm) {
-    return fetch(
-      `https://api.spotify.com/v1/search?type=track&q=${searchTerm}`,
-      { headers: { Authorization: `Bearer ${this.getAccessToken()}` } }
-    )
-      .then(response => {
-        if (!response.ok) throw Error(response.statusText);
-        return response;
-      })
-      .then(response => response.json())
+    return fetch(`${baseURL}/search?type=track&q=${searchTerm}`, {
+      headers: { Authorization: `Bearer ${this.getAccessToken()}` }
+    })
+      .then(this.handleResponse, this.handleNetworkError)
       .then(jsonResponse => {
-        console.log(jsonResponse);
         if (jsonResponse.tracks.items.length > 0) {
           return jsonResponse.tracks.items.map(track => ({
             id: track.id,
@@ -43,6 +48,41 @@ const Spotify = {
           return [];
         }
       });
+  },
+
+  savePlaylist(playlistName, trackURIs) {
+    if (!playlistName || !trackURIs || trackURIs.length === 0) return;
+
+    const token = this.getAccessToken();
+    const headers = { Authorization: `Bearer ${token}` };
+    let userID = '';
+    let playlistID = '';
+    return fetch(`${baseURL}/me`, { headers: headers })
+      .then(this.handleResponse, this.handleNetworkError)
+      .then(jsonResponse => (userID = jsonResponse.id))
+      .then(() => {
+        return fetch(`${baseURL}/users/${userID}/playlists`, {
+          method: 'POST',
+          headers: headers,
+          body: JSON.stringify({
+            name: playlistName
+          })
+        });
+      })
+      .then(this.handleResponse, this.handleNetworkError)
+      .then(jsonResponse => (playlistID = jsonResponse.id))
+      .then(() => {
+        return fetch(`${baseURL}/users/${userID}/playlists/${playlistID}/tracks`, {
+          method: 'POST',
+          headers: headers,
+          body: JSON.stringify({
+            uris: trackURIs
+          })
+        });
+      })
+      .then(this.handleResponse)
+      .then(jsonResponse => playlistID = jsonResponse.snapshot_id)
+      
   }
 };
 
